3bbda6592da3c0655b36bb0de6eb3d4244a14dd6
jackie_zhao <zrbjava@gmail.com>
2018-06-10 18:46:24 +0800

    更改domain

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,5 @@
-// export const domain = "http://192.168.1.138/new_palmar/public/wechat/index.php/"
-// export const domain = "http://bobo.com/new_palmar/public/apitest/index.php"
-export const domain = "http://bobo.com/new_palmar/public/wechat/index.php/"
+export const domain = "http://192.168.1.138/new_palmar/public/wechat/index.php/"
+// export const domain = "http://bobo.com/new_palmar/public/wechat/index.php/" 
 export default {
     // 这里是接口列表
     index: "Apivote/index", //活动首页
